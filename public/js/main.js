72e362588f68f83ab34fb4a6c4818208ac609674
Maciej Krüger <mkg20001@gmail.com>
2019-09-04 17:55:22 +0200

    feat: pull fontawesome via npm

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,8 @@
+import 'fontawesome-4.7/css/font-awesome.min.css'
+import '../css/animate.css'
+import '../css/raleway.css'
+import '../css/language-switcher.css'
+
 window.$ = require('jquery')
 
 $(document).ready(function () {
